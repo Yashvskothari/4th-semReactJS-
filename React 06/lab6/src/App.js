cd9f254c28c33015e150fb62f88933e027d2e7ca
Yashvskothari <yashkothari2605@gmail.com>
2026-03-17 10:40:39 +0530

    new project

---
+++ React 06/lab6/src/App.js
@@ -0,0 +1,13 @@
+
+import React from "react";
+import RandomNumberGenerator from "./randomNumberGenerator";
+
+function App() {
+  return (
+    <div>
+      <RandomNumberGenerator />
+    </div>
+  );
+}
+
+export default App;
